0e7e518b42de829a8ca93fa3b6a031da4a9ab601
no1hereDev <147976037+no1hereDev@users.noreply.github.com>
2026-04-29 10:18:45 +0700

    update init.lua

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 _G.Settings = {
 }
 
-local Obvion = {
+local SkibidiToilet = {
     Version = "v0.0.1",
     ClientStartTime = os.time(),
     DiscordLink = "https://discord.gg/jnHfKn"
@@ -14,7 +14,7 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Obvion Hub ",
+    Title = "Skibidi Hub ",
     SubTitle = "By No1here",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 400),
@@ -25,18 +25,7 @@ local Window = Fluent:CreateWindow({
 
 local Tabs = {
     Debug = Window:AddTab({ Title = "Debug", icon = "" }),
-    ServerStatus = Window:AddTab({ Title = "Server Status", Icon = "" }),
-    LocalPlayer = Window:AddTab({ Title = "Local Player", Icon = "" }),
-    Servers = Window:AddTab({ Title = "Servers", Icon = "" }),
-    SettingFarm = Window:AddTab({ Title = "Setting Farm", Icon = "" }),
-    Farm = Window:AddTab({ Title = "Farm", Icon = "" }),
-    SubFarm = Window:AddTab({ Title = "Sub Farm", Icon = "" }),
-    OtherFarm = Window:AddTab({ Title = "Other Farm/Get Items", Icon = "" }),
-    RaidsAndFruits = Window:AddTab({ Title = "Raids/Fruits", Icon = "" }),
-    RaceV4 = Window:AddTab({ Title = "Race V4", Icon = "" }),
-    SettingSeaEvents = Window:AddTab({ Title = "Setting Sea Events", Icon = "" }),
-    SeaEvents = Window:AddTab({ Title = "Sea Events", Icon = "" }),
-    About = Window:AddTab({ Title = "About Us", Icon = "" }),
+    Farm = Window:AddTab({ Title = "Farm", icon = "" }),
 }
 
 local Options = Fluent.Options

4ef7b4baa90a93f636b34b4dfa10b24dbd7f6263
no1hereDev <147976037+no1hereDev@users.noreply.github.com>
2026-04-30 02:34:31 +0700

    update init.lua

--- init.lua
+++ init.lua
@@ -33,7 +33,7 @@ local RunService = game:GetService("RunService")
 local TweenService = game:GetService("TweenService")
 spawn(function()
   Tabs.Debug:AddButton({
-    Title = "Refresh Data",
+    Title = "Refresh Datjansa",
     Description = "",
     Callback = function()
       game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("TraitReroll"):FireServer()
